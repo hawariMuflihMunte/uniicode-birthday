792a8da5a8a0e4ca62b16f436c7da7ab61949fa3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-23 18:15:40 +0700

    Delete `HelloWorld.vue`

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
+import UniicodeLogo from './components/UniicodeLogo.vue';
 </script>
 
 <template>
-  
+  <div class="container">
+    <div class="centered">
+      <UniicodeLogo />
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style>
+.container {
+  max-width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
 
+@media (max-width: 576px) {
+  .container {
+    max-width: 96%;
+  }
+}
+
+.centered {
+  display: flex;
+  place-content: center;
+  place-items: center;
+}
 </style>
