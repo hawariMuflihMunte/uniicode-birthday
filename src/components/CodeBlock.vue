8d7dc599cd40dac71c759ddc1b85dbed120cd776
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-23 20:07:39 +0700

    Create `CodeBlock.vue` component

---
+++ src/components/CodeBlock.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  language: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <pre><code :class="`language-${props.language}`">
+    <slot></slot>
+  </code></pre>
+</template>
+
+<style>
+
+</style>
