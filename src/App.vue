e08b4d54529fe3d0f11edf5fdc671188c5036e79
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-23 20:52:54 +0700

    Modularize data

--- src/App.vue
+++ src/App.vue
@@ -2,60 +2,10 @@
 import UniicodeLogo from './components/UniicodeLogo.vue';
 import CodeBlock from './components/CodeBlock.vue';
 
-const codes = {
-  js: () => {
-    return `document.write('Aku Cinta UNIICODE')`.trim();
-  },
-  c: () => {
-    return `#include <stdio.h>
+// @ts-ignore
+import { codes } from './utils/local-data.js';
 
-    int main () {
-      printf("Aku Cinta UNIICODE\n");
-
-      return 0;
-    }
-    `.trim();
-  },
-  cpp: () => {
-    return `#include <iostream>
-    using namespace std;
-
-    int main() {
-      cout << "Aku cinta UNIICODE";
-
-      return 0;
-    }
-    `.trim();
-  },
-  java: () => {
-    return `public class Main {
-      public static void main(String[] args) {
-        System.out.println("Aku Cinta UNIICODE");
-      }
-    }`.trim();
-  },
-  python: () => {
-    return `print('Aku Cinta UNIICODE')`.trim();
-  },
-  ruby: () => {
-    return `puts "Aku Cinta UNIICODE"`.trim();
-  },
-  go: () => {
-    return `package main
-
-    import "fmt"
-
-    func main() {
-        fmt.Println("Aku Cinta UNIICODE")
-    }
-    `.trim();
-  },
-  haskell: () => {
-    return `main :: IO()
-    main = putStrLn "Aku Cinta UNIICODE"
-    `.trim();
-  }
-}
+const myCodes: any[] = codes;
 </script>
 
 <template>
@@ -64,7 +14,7 @@ const codes = {
       <UniicodeLogo />
     </div>
     <main>
-      <template v-for="(code, language) in codes" :key="language">
+      <template v-for="(code, language) in myCodes" :key="language">
         <CodeBlock :language="language">
           {{ code() }}
         </CodeBlock>
